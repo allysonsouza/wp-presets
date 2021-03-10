d56f11a793de7e28fc23b619947ccf162dbd6d3b
Felipe Santos <info@felipels.com>
2021-03-10 00:53:38 +0000

    Create core user meta fields - Closes #7

---
+++ src/metabox/core/user.php
@@ -0,0 +1,53 @@
+<?php
+
+$prefix_meta = $prefix . 'core_user_';
+
+/**
+ * Initiate the metabox
+ */
+$cmb = new_cmb2_box( array(
+    'id'            => $prefix_meta . 'metabox',
+    'title'         => __( '[Core] Current user', 'wppresets' ),
+    'object_types'  => array( 'wppresets_presets', ), // Post type
+    'context'       => 'normal',
+    'priority'      => 'high',
+    'show_names'    => true, // Show field names on the left
+    // 'cmb_styles' => false, // false to disable the CMB stylesheet
+    // 'closed'     => true, // Keep the metabox closed by default
+) ); 
+
+$cmb->add_field( array(
+	'name'    => __( 'First Name', 'wppresets' ),
+	'id'      => $prefix_meta . 'first_name',
+	'type'    => 'text',
+) );
+
+$cmb->add_field( array(
+	'name'    => __( 'Last Name', 'wppresets' ),
+	'id'      => $prefix_meta . 'last_name',
+	'type'    => 'text',
+) );
+
+$cmb->add_field( array(
+	'name'    => __( 'Nickname', 'wppresets' ),
+	'id'      => $prefix_meta . 'nickname',
+	'type'    => 'text',
+) );
+
+$cmb->add_field( array(
+	'name' => __( 'Website', 'wppresets' ),
+	'id'   => $prefix_meta . 'website',
+	'type' => 'text_url',
+) );
+
+$cmb->add_field( array(
+	'name' => __( 'Email', 'wppresets' ),
+	'id'   => $prefix_meta . 'email',
+	'type' => 'text_email',
+) );
+
+$cmb->add_field( array(
+	'name' => __( 'Bio', 'wppresets' ),
+	'id' => $prefix_meta . 'bio',
+	'type' => 'textarea'
+) );
--- src/metabox/presets-options.php
+++ src/metabox/presets-options.php
@@ -7,27 +7,12 @@ if ( file_exists( dirname( __FILE__ ) . '/../../vendors/cmb2/init.php' ) ) {
 	require_once dirname( __FILE__ ) . '/../../vendors/cmb2/init.php';
 }
 
-/**
- * Define the metabox and field configurations.
- */
-function wppresets_settings() {
+function wppresets_options() {
 
-	/**
-	 * Initiate the metabox
-	 */
-	$cmb = new_cmb2_box( array(
-		'id'            => 'wppresets_settings_metabox',
-		'title'         => __( 'Settings', 'wppresets' ),
-		'object_types'  => array( 'presets', ), // Post type
-		'context'       => 'normal',
-		'priority'      => 'high',
-		'show_names'    => true, // Show field names on the left
-		// 'cmb_styles' => false, // false to disable the CMB stylesheet
-		// 'closed'     => true, // Keep the metabox closed by default
-	) );
+	$prefix = 'wppresets_';
 
+	include_once plugin_dir_path( __FILE__ ) . 'core/user.php';
 
+}
 
-	}
-
-add_action( 'cmb2_admin_init', 'wppresets_settings' );
+add_action( 'cmb2_admin_init', 'wppresets_options' );
