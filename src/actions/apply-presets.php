c2ad57b38d9cacbb630574f0434fd1945a2509d5
Felipe Santos <info@felipels.com>
2021-04-07 02:18:38 +0100

    New action confirmation notice

--- src/actions/apply-presets.php
+++ src/actions/apply-presets.php
@@ -10,7 +10,7 @@ function get_presets_meta( $prefix, $field ) {
 
 function presets_admin_notice__success() {    ?>
 	<div class="notice notice-success is-dismissible">
-		<p><?php _e( 'The settings were applied as expected! ', 'presets' ); ?></p>
+		<p><?php _e( 'The settings were applied as expected!', 'presets' ) . var_dump( get_post_meta( $_GET['presets-applied'], '_' ) ); ?></p>
 	</div>
 	<?php
 }
@@ -19,11 +19,21 @@ function apply_presets() {
 	if ( isset( $_GET['presets-trigger'] ) && current_user_can( 'manage_options' ) ) {
 
 		presets_core_user_apply_meta();
+		wp_redirect( 'edit.php?post_type=presets&presets-applied=' . $_GET['presets-trigger'], 301 );
+		exit;
 
-		add_action( 'admin_notices', 'presets_admin_notice__success' );
+	}
+
+}
+
+function presets_confirmation() {
+	if ( isset( $_GET['presets-applied'] ) ) {
+
+			add_action( 'admin_notices', 'presets_admin_notice__success' );
 
 	}
 
 }
 
 add_action( 'admin_head', 'apply_presets' );
+add_action( 'admin_head', 'presets_confirmation' );
