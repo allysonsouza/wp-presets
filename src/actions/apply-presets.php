04a20fa87383c4e40c86064027d8dd73027ae741
Felipe Santos <info@felipels.com>
2021-04-08 23:48:30 +0100

    Sanitize $_GET

--- src/actions/apply-presets.php
+++ src/actions/apply-presets.php
@@ -3,8 +3,7 @@
 require_once plugin_dir_path( __FILE__ ) . 'core/user.php';
 
 function get_presets_meta( $prefix, $field ) {
-	$selected_presets_id = $_GET['presets-trigger'];
-
+	$selected_presets_id = filter_var( $_GET['presets-trigger'], FILTER_SANITIZE_NUMBER_INT );
 	return get_post_meta( $selected_presets_id, 'presets_' . $prefix . $field, true );
 }
 
@@ -16,13 +15,20 @@ function presets_admin_notice__success() {    ?>
 }
 
 function apply_presets() {
-	if ( isset( $_GET['presets-trigger'] ) && current_user_can( 'manage_options' ) ) {
 
-		presets_core_user_apply_meta();
-
-		add_action( 'admin_notices', 'presets_admin_notice__success' );
-
+	if ( ! isset( $_GET['presets-trigger'] ) || ! current_user_can( 'manage_options' ) ) {
+		return;
 	}
+
+	$selected_presets_id = filter_var( $_GET['presets-trigger'], FILTER_SANITIZE_NUMBER_INT );
+
+	if ( get_post_type( $selected_presets_id ) !== 'presets' ) {
+		return;
+	}
+
+	presets_core_user_apply_meta();
+
+	add_action( 'admin_notices', 'presets_admin_notice__success' );
 
 }
 
