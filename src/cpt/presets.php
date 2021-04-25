9de142c544ba9b483c81e54998c12cdab6d56a09
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2021-04-25 05:52:09 +0100

    Fix 'presets' CPT labels set

--- src/cpt/presets.php
+++ src/cpt/presets.php
@@ -1,40 +1,40 @@
 <?php
 
 /**
- * Create CPT 'presets'
+ * Create CPT 'presets'.
  *
  * @return void
  */
 function presets_cpt() {
 
 	$labels = array(
-		'name'                    => _x( 'Presets', 'Post Type General Name', 'presets' ),
-		'singular_name'           => _x( 'Preset', 'Post Type Singular Name', 'presets' ),
-		'menu_name'               => __( 'Presets', 'presets' ),
-		'name_admin_bar'          => __( 'Preset', 'presets' ),
-		'archives'                => __( 'Preset Archives', 'presets' ),
-		'attributes'              => __( 'Preset Attributes', 'presets' ),
-		'parent_Preset_colon'     => __( 'Parent Preset:', 'presets' ),
-		'all_Presets'             => __( 'All Presets', 'presets' ),
-		'add_new_Preset'          => __( 'Add New Preset', 'presets' ),
-		'add_new'                 => __( 'Add New', 'presets' ),
-		'new_Preset'              => __( 'New Preset', 'presets' ),
-		'edit_Preset'             => __( 'Edit Preset', 'presets' ),
-		'update_Preset'           => __( 'Update Preset', 'presets' ),
-		'view_Preset'             => __( 'View Preset', 'presets' ),
-		'view_Presets'            => __( 'View Presets', 'presets' ),
-		'search_Presets'          => __( 'Search Preset', 'presets' ),
-		'not_found'               => __( 'Not found', 'presets' ),
-		'not_found_in_trash'      => __( 'Not found in Trash', 'presets' ),
-		'featured_image'          => __( 'Featured Image', 'presets' ),
-		'set_featured_image'      => __( 'Set featured image', 'presets' ),
-		'remove_featured_image'   => __( 'Remove featured image', 'presets' ),
-		'use_featured_image'      => __( 'Use as featured image', 'presets' ),
-		'insert_into_Preset'      => __( 'Insert into Preset', 'presets' ),
-		'uploaded_to_this_Preset' => __( 'Uploaded to this Preset', 'presets' ),
-		'Presets_list'            => __( 'Presets list', 'presets' ),
-		'Presets_list_navigation' => __( 'Presets list navigation', 'presets' ),
-		'filter_Presets_list'     => __( 'Filter Presets list', 'presets' ),
+		'name'                  => _x( 'Presets', 'Post Type General Name', 'presets' ),
+		'singular_name'         => _x( 'Preset', 'Post Type Singular Name', 'presets' ),
+		'menu_name'             => __( 'Presets', 'presets' ),
+		'name_admin_bar'        => __( 'Preset', 'presets' ),
+		'archives'              => __( 'Preset Archives', 'presets' ),
+		'attributes'            => __( 'Preset Attributes', 'presets' ),
+		'parent_item_colon'     => __( 'Parent Preset:', 'presets' ),
+		'all_items'             => __( 'All Presets', 'presets' ),
+		'add_new_item'          => __( 'Add New Preset', 'presets' ),
+		'add_new'               => __( 'Add New', 'presets' ),
+		'new_item'              => __( 'New Preset', 'presets' ),
+		'edit_item'             => __( 'Edit Preset', 'presets' ),
+		'update_item'           => __( 'Update Preset', 'presets' ),
+		'view_item'             => __( 'View Preset', 'presets' ),
+		'view_items'            => __( 'View Presets', 'presets' ),
+		'search_items'          => __( 'Search Presets', 'presets' ),
+		'not_found'             => __( 'Not found', 'presets' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'presets' ),
+		'featured_image'        => __( 'Featured Image', 'presets' ),
+		'set_featured_image'    => __( 'Set featured image', 'presets' ),
+		'remove_featured_image' => __( 'Remove featured image', 'presets' ),
+		'use_featured_image'    => __( 'Use as featured image', 'presets' ),
+		'insert_into_item'      => __( 'Insert into Preset', 'presets' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Preset', 'presets' ),
+		'items_list'            => __( 'Presets list', 'presets' ),
+		'items_list_navigation' => __( 'Presets list navigation', 'presets' ),
+		'filter_items_list'     => __( 'Filter Presets list', 'presets' ),
 	);
 	$args   = array(
 		'label'               => __( 'Preset', 'presets' ),
