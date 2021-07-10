0bf6a7c59a5810bc6c71e19a002453768138dd8c
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2021-07-10 16:07:14 +0100

    Notes feature initial setup
    Closes #15

--- modules/modules.php
+++ modules/modules.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once plugin_dir_path( __FILE__ ) . 'woocommerce/module.php';
+require_once plugin_dir_path( __FILE__ ) . 'notes/module.php';
--- src/actions/apply-presets.php
+++ src/actions/apply-presets.php
@@ -71,7 +71,8 @@ function presets_admin_notice__success() {
 
 	?>
 		<div class="notice notice-success is-dismissible">
-			<p><?php _e( 'The settings were applied as expected! ' ); ?></p>
+		<p><?php _e( 'The settings were applied as expected! ' ); ?></p>
+		<?php do_action( 'presets_admin_notice_sucess' ); ?>
 		</div>
 	<?php
 }
