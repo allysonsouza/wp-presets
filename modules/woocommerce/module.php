2957b474ccfda1a6a1b1f2f558dcda4699b333d3
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2022-03-05 01:21:58 +0000

    Initial modules code improvements

--- modules/woocommerce/module.php
+++ modules/woocommerce/module.php
@@ -4,8 +4,7 @@ function presets_module_woocommerce_activate() {
 
 	if ( class_exists( 'woocommerce' ) ) {
 
-		require_once plugin_dir_path( __FILE__ ) . 'fields/general-settings.php';
-		require_once plugin_dir_path( __FILE__ ) . 'actions/general-settings.php';
+		require_once plugin_dir_path( __FILE__ ) . 'GeneralSettings.php';
 
 	}
 
