a0e8bd00bef3cee70f2523ad4fe80320df3450ab
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2022-08-01 19:10:30 -0300

    WIP: Create fields for Plugins action

--- modules/core/Plugins.php
+++ modules/core/Plugins.php
@@ -1,6 +1,6 @@
 <?php
 
-class Plugins extends ActionBase {
+class CorePlugins extends ActionBase {
 
 	private function getAvaliablePlugins() {
 
@@ -14,9 +14,9 @@ class Plugins extends ActionBase {
 	
 		foreach ( $plugins as $plugin => $meta ) {
 	
-			if ( presets_plugin_filename() !== $plugin && ! in_array( $plugin, presets_get_option_skipped_plugins(), true ) ) {
+		//	if ( presets_plugin_filename() !== $plugin && ! in_array( $plugin, presets_get_option_skipped_plugins(), true ) ) {
 				$avaliable_plugins[ $plugin ] = $meta['Name'];
-			}
+	//		}
 		}
 	
 		return $avaliable_plugins;
@@ -25,89 +25,34 @@ class Plugins extends ActionBase {
 
 	public function createFields($metabox, $group) {
 
-
-	$prefix_meta = 'presets_core_plugins_';
-
-	/**
-	 * Initiate the metabox
-	 */
-
-	$cmb = new_cmb2_box(
-		array(
-			'id'           => $prefix_meta . 'metabox',
-			'title'        => __( 'Installed plugins', 'presets' ),
-			'object_types' => array( 'presets' ), // Post type
-			'context'      => 'normal',
-			'priority'     => 'low',
-			'show_names'   => true, // Show field names on the left
-		)
-	);
-
-	$cmb->add_field(
-		array(
-			'name'    => __( 'Activate Plugins', 'presets' ),
-			'desc'    => __( 'Select the plugins that should get activated when this preset gets triggered. If the plugin is not selected, it is going to be deactivated. You are able to <a href="edit.php?post_type=presets&page=presets_advanced_settings">skip specific plugins from being deactivated/activated on the plugin settings</a>.', 'presets' ),
-			'id'      => $prefix_meta . 'activate',
-			'type'    => 'multicheck',
-			'options' => presets_get_avaliable_plugins(),
-		),
-	);
-
 		$classes = $this->slug . ' hide';
+
+		/**
+		 * Initiate the metabox
+		 */
 
 		$metabox->add_group_field( 
 			$group,
 			array(
-			'name' => __( 'Site Title', 'presets' ),
-			'id'   => $this->slug . '_blogname',
-			'type' => 'text',
-			'classes' => $classes,
-			)
-		);
-
-		$metabox->add_group_field( $group,
-			array(
-				'name' => __( 'Tagline', 'presets' ),
-				'id'   => $this->slug . '_blogdescription',
-				'type' => 'text',
+				'name'    => __( 'Deactivate Plugins', 'presets' ),
+				'desc'    => __( 'Select the plugins that should get deactivated when this preset gets triggered.', 'presets' ),
+				'id'      => $this->slug . 'deactivate',
+				'type'    => 'multicheck',
+				'options' => $this->getAvaliablePlugins(),
 				'classes' => $classes,
-			)
-		);
-
-		$metabox->add_group_field( $group,
-			array(
-			'name' => __( 'Administration Email Address', 'presets' ),
-			'id'   => $this->slug . '_admin_email',
-			'type' => 'text_email',
-			'classes' => $classes,
-		)
-		);
-
-		$metabox->add_group_field( $group,
-			array(
-			'name'             => __( 'Anyone can register', 'presets' ),
-			'id'               => $this->slug . '_users_can_register',
-			'type'             => 'select',
-			'show_option_none' => ' ',
-			'default'          => 'custom',
-			'options'          => array(
-				0 => __( 'No', 'presets' ),
-				1 => __( 'Yes', 'presets' ),
 			),
-			'classes' => $classes,
-		)
 		);
 
-		$metabox->add_group_field( $group,
+		$metabox->add_group_field( 
+			$group,
 			array(
-			'name'             => __( 'Site Language', 'presets' ),
-			'id'               => $this->slug . '_WPLANG',
-			'type'             => 'select',
-			'show_option_none' => ' ',
-			'default'          => 'custom',
-			'options'          => $this->languageOptions(),
-			'classes' => $classes,
-		)
+				'name'    => __( 'Activate Plugins', 'presets' ),
+				'desc'    => __( 'Select the plugins that should get activated when this preset gets triggered.', 'presets' ),
+				'id'      => $this->slug . 'activate',
+				'type'    => 'multicheck',
+				'options' => $this->getAvaliablePlugins(),
+				'classes' => $classes,
+			),
 		);
 
 	}
@@ -115,8 +60,6 @@ class Plugins extends ActionBase {
 	public function applyAction($id) {
 
 		$entries = get_post_meta( $id, 'preset_actions_repeat_group', true );
-
-		error_log(print_r($entries, true));
 
 		foreach ( (array) $entries as $key => $entry ) {
 			
@@ -170,8 +113,8 @@ class Plugins extends ActionBase {
 	}
 }
 
-$obj = new CoreGeneralSettings(
+$obj = new CorePlugins(
 	'core-plugins',
 	__( '[Core] Plugins', 'presets' ),
-	__( 'Plugins to activate/deactivate for the site', 'presets' )
+	__( 'Plugins to activate/deactivate for WordPress', 'presets' )
 );
